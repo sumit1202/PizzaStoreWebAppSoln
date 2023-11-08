89cf9b59072555e223feddf1ddecea1d26944667
sumitkumar <sumitkumar1202@gmail.com>
2023-11-08 09:54:18 +0530

    commit1

---
+++ PizzaStoreWebAppProject/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PizzaStoreWebAppProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
